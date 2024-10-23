1fc69200fbc10b60a5f1b8293413662b2c6c4f41
silentgraphic <kierannolan1995@gmail.com>
2024-10-23 11:59:57 +0100

    Setup basic redux store

---
+++ src/slices/calculator.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+};
+
+const calculatorSlice = createSlice({
+    name: "calculatorSlice",
+    initialState,
+    reducers: {
+
+    }
+});
+
+export default calculatorSlice.reducer;
