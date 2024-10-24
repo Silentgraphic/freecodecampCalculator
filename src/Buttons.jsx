4d911baf71047aaaaf3c02cb23e6bf8309bccc98
silentgraphic <kierannolan1995@gmail.com>
2024-10-24 11:42:06 +0100

    Set logic for calculator

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -6,16 +6,16 @@ function Buttons() {
 
     return (
         <div id="buttons">
-            <button id="zero" className="nums" onClick={() => dispatch(addToCalc(0))}>0</button>
-            <button id="one" className="nums" onClick={() => dispatch(addToCalc(1))}>1</button>
-            <button id="two" className="nums" onClick={() => dispatch(addToCalc(2))}>2</button>
-            <button id="three" className="nums" onClick={() => dispatch(addToCalc(3))}>3</button>
-            <button id="four" className="nums" onClick={() => dispatch(addToCalc(4))}>4</button>
-            <button id="five" className="nums" onClick={() => dispatch(addToCalc(5))}>5</button>
-            <button id="six" className="nums" onClick={() => dispatch(addToCalc(6))}>6</button>
-            <button id="seven" className="nums" onClick={() => dispatch(addToCalc(7))}>7</button>
-            <button id="eight" className="nums" onClick={() => dispatch(addToCalc(8))}>8</button>
-            <button id="nine" className="nums" onClick={() => dispatch(addToCalc(9))}>9</button>
+            <button id="zero" className="nums" onClick={() => dispatch(addToCalc("0"))}>0</button>
+            <button id="one" className="nums" onClick={() => dispatch(addToCalc("1"))}>1</button>
+            <button id="two" className="nums" onClick={() => dispatch(addToCalc("2"))}>2</button>
+            <button id="three" className="nums" onClick={() => dispatch(addToCalc("3"))}>3</button>
+            <button id="four" className="nums" onClick={() => dispatch(addToCalc("4"))}>4</button>
+            <button id="five" className="nums" onClick={() => dispatch(addToCalc("5"))}>5</button>
+            <button id="six" className="nums" onClick={() => dispatch(addToCalc("6"))}>6</button>
+            <button id="seven" className="nums" onClick={() => dispatch(addToCalc("7"))}>7</button>
+            <button id="eight" className="nums" onClick={() => dispatch(addToCalc("8"))}>8</button>
+            <button id="nine" className="nums" onClick={() => dispatch(addToCalc("9"))}>9</button>
             <button id="add" onClick={() => dispatch(addToCalc("+"))}>+</button>
             <button id="subtract" onClick={() => dispatch(addToCalc("-"))}>-</button>
             <button id="multiply" onClick={() => dispatch(addToCalc("+"))}>*</button>
--- src/slices/calculator.js
+++ src/slices/calculator.js
@@ -1,22 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentCalc: [],
+    currentCalc: [""],
     calcOutput: 0
 };
 
 function calculate(state) {
-    const test = state.currentCalc;
-    console.log(test.toString());
+    const calcArry = state.currentCalc;
+    return calcArry.reduce((acc, val, index) => {
+        const nextVal = parseFloat(calcArry[index + 1]);
+        switch (val[0]) {
+            case "+":
+                acc += nextVal;
+                break;
+            case "-":
+                acc -= nextVal;
+                break;
+            case "/":
+                acc /= nextVal;
+                break;
+            case "*":
+                acc *= nextVal;
+                break;
+            default:
+                break;
+        }
+        return acc;
+    }, parseFloat(calcArry[0]));
 }
 
 const calculatorSlice = createSlice({
     name: "calculatorSlice",
     initialState,
     reducers: {
-        addToCalc: (state, action) => { state.currentCalc.push(action.payload); },
-        clearCalc: (state) => { state.currentCalc = []; },
-        submitCalc: (state) => { calculate(state); }
+        addToCalc: (state, action) => {
+            const lastItem = state.currentCalc[state.currentCalc.length - 1];
+            if ((parseInt(action.payload) || action.payload === ".") && (parseInt(lastItem[0]) || lastItem === "")) {
+                if (lastItem[lastItem.length - 4] !== ".") state.currentCalc[state.currentCalc.length - 1] += action.payload;
+            } else if (lastItem !== "") {
+                state.currentCalc.push(action.payload);
+            }
+        },
+        clearCalc: (state) => { state.currentCalc = [""]; },
+        submitCalc: (state) => {
+            state.calcOutput = calculate(state);
+            state.currentCalc = [state.calcOutput];
+            console.log(state.calcOutput);
+        }
     }
 });
 
